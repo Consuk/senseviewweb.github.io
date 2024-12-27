7b9f1e43cade5364ffb5d0967b9955ea0310577d
Consuk <0228053@up.edu.mx>
2024-12-27 12:20:55 -0600

    Update github pages config2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import Home from "./Pages/Home/home";
 import Prices from "./Pages/Prices/Prices.js";
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        {/* Navbar */}
-        <Navbar />
-        <div className="main-content">
-          {/* Other Routes */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/pricing" element={<Prices />} />
-            <Route path="/faq" element={<div>Preguntas Frecuentes</div>} />
-            <Route path="/contact" element={<div>Contacto</div>} />
-            <Route path="/about" element={<div>Sobre Nosotros</div>} />
-          </Routes>
-        </div>
-        {/* Footer */}
-        <Footer />
-      </Router>
+      {/* Navbar */}
+      <Navbar />
+      <div className="main-content">
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Prices />} />
+          <Route path="/faq" element={<div>Preguntas Frecuentes</div>} />
+          <Route path="/contact" element={<div>Contacto</div>} />
+          <Route path="/about" element={<div>Sobre Nosotros</div>} />
+        </Routes>
+      </div>
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
 
-
 export default App;
-
